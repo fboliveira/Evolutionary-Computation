6d8791060b547eda1e9a35cf1809977d5109733e
Fernando B Oliveira <fboliveira25@gmail.com>
2025-10-14 20:37:53 -0300

    updated: return best cost and best individual

--- Codes/TestFunction/problem.py
+++ Codes/TestFunction/problem.py
@@ -1,3 +1,5 @@
+import math
+
 def calcular_fo_sum_square(lista_variaveis):
 
     # -- Calcular a função objetivo: sum(Xi^2)
@@ -10,9 +12,25 @@ def calcular_fo_sum_square(lista_variaveis):
 def avaliar_populacao(populacao_inicial):
 
     populacao = []
+    melhor_custo = math.inf
+    melhor_individuo = []
 
     for individuo in populacao_inicial:
         custo = calcular_fo_sum_square(individuo)
         populacao.append((individuo, custo))
+
+        if custo < melhor_custo:
+            melhor_custo = custo
+            melhor_individuo = individuo.copy()
+
     
-    return populacao
+    return populacao, melhor_custo, melhor_individuo
+
+def valida_min_max(valor, min, max):
+    if ( valor < min ):
+        valor = min
+
+    if ( valor > max ):
+        valor = max    
+    
+    return valor
